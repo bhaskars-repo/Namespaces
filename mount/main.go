708617e04d8cf212c794a7f80e181749fea78548
bhaskars-repo <bswamina@gmail.com>
2020-03-20 11:08:18 -0400

    Namespaces in Go

---
+++ mount/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+    "log"
+    "os"
+    "os/exec"
+    "syscall"
+)
+
+func createTxtFile() {
+    f, err := os.Create("/tmp/leopard.txt")
+    if err != nil {
+        panic(err)
+    }
+
+    defer f.Close()
+
+    _, err = f.WriteString("leopard")
+    if err != nil {
+        panic(err)
+    }
+}
+
+func execContainerShell() {
+    log.Printf("Ready to exec container shell ...\n")
+
+    if err := syscall.Sethostname([]byte("leopard")); err != nil {
+        panic(err)
+    }
+
+    mf := uintptr(syscall.MS_NODEV)
+    if err := syscall.Mount("proc", "/proc", "proc", mf, ""); err != nil {
+        panic(err)
+    }
+
+    mf = uintptr(syscall.MS_PRIVATE | syscall.MS_REC)
+    if err := syscall.Mount("", "/", "", mf, ""); err != nil {
+        panic(err)
+    }
+
+    mf = uintptr(syscall.MS_NODEV)
+    if err := syscall.Mount("tmpfs", "/tmp", "tmpfs", mf, ""); err != nil {
+        panic(err)
+    }
+
+    createTxtFile()
+
+    const sh = "/bin/sh"
+
+    env := os.Environ()
+    env = append(env, "PS1=-> ")
+
+    if err := syscall.Exec(sh, []string{""}, env); err != nil {
+        panic(err)
+    }
+}
+
+func main() {
+    log.Printf("Starting process %s with args: %v\n", os.Args[0], os.Args)
+
+    const clone = "CLONE"
+
+    if len(os.Args) > 1 && os.Args[1] == clone {
+        execContainerShell()
+        os.Exit(0)
+    }
+
+    log.Printf("Ready to run command ...\n")
+
+    cmd := exec.Command(os.Args[0], []string{clone}...)
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+    cmd.SysProcAttr = &syscall.SysProcAttr{
+        Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNS | syscall.CLONE_NEWPID,
+        UidMappings: []syscall.SysProcIDMap{
+            {ContainerID: 0, HostID: 0, Size: 1},
+        },
+        GidMappings: []syscall.SysProcIDMap{
+            {ContainerID: 0, HostID: 0, Size: 1},
+        },
+    }
+
+    if err := cmd.Run(); err != nil {
+        panic(err)
+    }
+}
